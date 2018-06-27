d892407ebdd489adf3b55edd6ccc19fbb1853c33
Kocherlakota <srkocherlakota@deloitte.com>
2018-06-27 17:50:59 -0400

    some changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ My First Sucess Expiriment
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3001;
 
  app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
@@ -29,6 +30,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
